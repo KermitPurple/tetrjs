9e32cb6e555f1f4a3700bcf284ce4267135c509a
Shane McDonough <shanemcdman@gmail.com>
2021-02-28 23:07:49 -0800

    create an array of all pieces

--- pieces.js
+++ pieces.js
@@ -8,3 +8,41 @@ const CellType = {
     Z: 'Z',
     T: 'T',
 };
+
+const PIECES = [
+    [
+        [CellType.None, CellType.I, CellType.None, CellType.None],
+        [CellType.None, CellType.I, CellType.None, CellType.None],
+        [CellType.None, CellType.I, CellType.None, CellType.None],
+        [CellType.None, CellType.I, CellType.None, CellType.None],
+    ],
+    [
+        [CellType.O, CellType.O],
+        [CellType.O, CellType.O],
+    ],
+    [
+        [CellType.None, CellType.L, CellType.None],
+        [CellType.None, CellType.L, CellType.None],
+        [CellType.None, CellType.L, CellType.L],
+    ],
+    [
+        [CellType.None, CellType.J, CellType.None],
+        [CellType.None, CellType.J, CellType.None],
+        [CellType.J, CellType.J, CellType.None],
+    ],
+    [
+        [CellType.S, CellType.None, CellType.None],
+        [CellType.S, CellType.S, CellType.None],
+        [CellType.None, CellType.S, CellType.None],
+    ],
+    [
+        [CellType.None, CellType.None, CellType.Z],
+        [CellType.None, CellType.Z, CellType.Z],
+        [CellType.None, CellType.Z, CellType.None],
+    ],
+    [
+        [CellType.None, CellType.None, CellType.Z],
+        [CellType.None, CellType.Z, CellType.Z],
+        [CellType.None, CellType.Z, CellType.None],
+    ],
+];
