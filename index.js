dd58e4f73df01cfabcb1615172f0bfc45cd734fe
Shane McDonough <shanemcdman@gmail.com>
2021-02-28 23:02:47 -0800

    Add cell types and add classes that set color for each cell type

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const BOARD_SIZE = {x: 10, y: 20};
 const cells = document.querySelectorAll('.cell');
+
 function get_cell_at(x, y){
     return cells[y * BOARD_SIZE.x + x];
 }
---
+++ pieces.js
@@ -0,0 +1,10 @@
+const CellType = {
+    None: '',
+    I: 'I',
+    O: 'O',
+    L: 'L',
+    J: 'J',
+    S: 'S',
+    Z: 'Z',
+    T: 'T',
+};
